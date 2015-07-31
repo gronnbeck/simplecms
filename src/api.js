1e39cb42dbabe740db3a15fccc00903056da161b
Ken Grønnbeck <kengroenn@gmail.com>
2015-07-31 19:27:47 +0200

    Just moved shit to another file

---
+++ src/api.js
@@ -0,0 +1,67 @@
+const models = require('./models');
+
+const PageDefinition = models.PageDefinition.model;
+const PageFactory = models.PageFactory;
+
+const FrontPageDefiniton = new PageDefinition({
+  pageName: 'FrontPage',
+  attributes: [
+    { type: 'textField', attributeName: 'title', isRequired: true }
+  ]
+});
+
+models.PageDefinition.init(FrontPageDefiniton);
+
+const FrontPage = PageFactory(FrontPageDefiniton);
+
+const NameToPageMap = {
+  [FrontPageDefiniton.pageName.toLowerCase()]: FrontPage
+}
+
+const getPageDefinitions = (req, res) => {
+  PageDefinition
+    .find({})
+    .exec((err, defs) => {
+      if (err) {
+        let message = {
+            developerMessage: 'An unexpected error occured',
+            userMessage: 'Whops that shouldn\'t have happened. Please contact the developer',
+            error: err
+        }
+        console.error(JSON.stringify(message));
+        return res.status(500).send(message);
+      }
+
+      return res.send(defs);
+    })
+}
+
+const getPages = (req, res) => {
+  const { pageDef: pageDef, pageId: pageId } = req.params;
+  const notImplementedMessage = {
+    developerMessage: 'Sorry this as not been implemented yet',
+    userMessage: 'Sorry, resource not found',
+    error: null
+  }
+  if (pageDef === null || pageDef === undefined || pageId == null || pageId === undefined)
+    return res.status(404).send(notImplementedMessage);
+  else {
+    var page = NameToPageMap[pageDef.toLowerCase()]
+    if (page === null || page === undefined)
+      return res.status(404).send(notImplementedMessage);
+    return  page
+      .find({})
+      .exec((err, pages) => {
+        if (err) throw Error('What the error is not handled');
+        const id = parseInt(pageId);
+        if (pages.length < id) return res.status(404).send({ error: 'not found' });
+        return res.send(pages[id]);
+      });
+  }
+}
+
+
+module.exports = {
+  getPageDefinitions,
+  getPages
+}
--- src/simplecms.js
+++ src/simplecms.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const models = require('./models');
+const api = require('./api');
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/simplecms';
 const PORT = process.env.PORT || 8080;
@@ -9,20 +10,6 @@ mongoose.connect(MONGODB_URI);
 
 const app = express();
 
-const PageDefinition = models.PageDefinition.model;
-const PageFactory = models.PageFactory;
-
-const FrontPageDefiniton = new PageDefinition({
-  pageName: 'FrontPage',
-  attributes: [
-    { type: 'textField', attributeName: 'title', isRequired: true }
-  ]
-});
-
-const pageDefinition = FrontPageDefiniton;
-models.PageDefinition.init(pageDefinition);
-
-const FrontPage = PageFactory(FrontPageDefiniton);
 // var frontPage = new FrontPage({
 //     title: 'Hello World'
 // });
@@ -32,51 +19,8 @@ const FrontPage = PageFactory(FrontPageDefiniton);
 //   return console.log('Successfully saved FrontPage');
 // });
 
-const NameToPageMap = {
-  [FrontPageDefiniton.pageName.toLowerCase()]: FrontPage
-}
-
-app.get('/pageDefinitions', (req, res) => {
-  PageDefinition
-    .find({})
-    .exec((err, defs) => {
-      if (err) {
-        let message = {
-            developerMessage: 'An unexpected error occured',
-            userMessage: 'Whops that shouldn\'t have happened. Please contact the developer',
-            error: err
-        }
-        console.error(JSON.stringify(message));
-        return res.status(500).send(message);
-      }
-
-      return res.send(defs);
-    })
-});
-
-app.get('/pages/:pageDef?/:pageId?', (req, res) => {
-  const { pageDef: pageDef, pageId: pageId } = req.params;
-  const notImplementedMessage = {
-    developerMessage: 'Sorry this as not been implemented yet',
-    userMessage: 'Sorry, resource not found',
-    error: null
-  }
-  if (pageDef === null || pageDef === undefined || pageId == null || pageId === undefined)
-    return res.status(404).send(notImplementedMessage);
-  else {
-    var page = NameToPageMap[pageDef.toLowerCase()]
-    if (page === null || page === undefined)
-      return res.status(404).send(notImplementedMessage);
-    return  page
-      .find({})
-      .exec((err, pages) => {
-        if (err) throw Error('What the error is not handled');
-        const id = parseInt(pageId);
-        if (pages.length < id) return res.status(404).send({ error: 'not found' });
-        return res.send(pages[id]);
-      });
-  }
-});
+app.get('/pageDefinitions', api.getPageDefinitions);
+app.get('/pages/:pageDef?/:pageId?', api.getPages);
 
 const server = app.listen(PORT, () => {
   console.log('Server is running on ' + PORT);
